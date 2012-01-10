2e73bd1c7a405d0b0057c33a0fe9dd25baf8bd88
asier.zorrozua@gmail.com <asier.zorrozua@gmail.com@08813549-792b-7029-359e-9ef8cbd5d2c8>
2012-01-10 11:17:38 +0000

    Añadiendo los botones al juego.

--- trunk/aPoker/src/com/zorrozua/asier/APokerServer.java
+++ trunk/aPoker/src/com/zorrozua/asier/APokerServer.java
@@ -10,6 +10,7 @@ import org.anddev.andengine.engine.options.resolutionpolicy.RatioResolutionPolic
 import org.anddev.andengine.entity.scene.Scene;
 import org.anddev.andengine.entity.scene.background.ColorBackground;
 import org.anddev.andengine.entity.scene.background.SpriteBackground;
+import org.anddev.andengine.entity.sprite.AnimatedSprite;
 import org.anddev.andengine.entity.sprite.BaseSprite;
 import org.anddev.andengine.entity.sprite.Sprite;
 import org.anddev.andengine.entity.util.FPSLogger;
@@ -23,6 +24,7 @@ import org.anddev.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextur
 import org.anddev.andengine.opengl.texture.region.BaseTextureRegion;
 import org.anddev.andengine.opengl.texture.region.TextureRegion;
 import org.anddev.andengine.opengl.texture.region.TextureRegionFactory;
+import org.anddev.andengine.opengl.texture.region.TiledTextureRegion;
 import org.anddev.andengine.ui.activity.BaseGameActivity;
 
 import android.util.Log;
@@ -38,12 +40,12 @@ public class APokerServer extends BaseGameActivity{
 	//	private static final int CAMERA_WIDTH = 720;
 	//	private static final int CAMERA_HEIGHT = 480;
 
-	private static int cameraWidth;
-	private static int cameraHeight;
-
 	// ===========================================================
 	// Fields
 	// ===========================================================
+
+	private static int cameraWidth;
+	private static int cameraHeight;
 
 	private Camera mCamera;
 	private Scene mMainScene;
@@ -57,6 +59,10 @@ public class APokerServer extends BaseGameActivity{
 	Sprite backgroundSpriteSprite;
 	SpriteBackground backgroundSprite;
 
+	//Buttons
+	private BitmapTextureAtlas mButtonsTextureAtlas;
+	private HashMap<Button, TextureRegion> mButtonsTextureRegionMap;
+
 	// ===========================================================
 	// Constructors
 	// ===========================================================
@@ -65,7 +71,7 @@ public class APokerServer extends BaseGameActivity{
 	// Getter & Setter
 	// ===========================================================
 
-	public static void setCameraWidth(int cameraWidth) {
+	public void setCameraWidth(int cameraWidth) {
 		APokerServer.cameraWidth = cameraWidth;
 	}
 
@@ -73,7 +79,7 @@ public class APokerServer extends BaseGameActivity{
 		return cameraWidth;
 	}
 
-	public static void setCameraHeight(int cameraHeight) {
+	public void setCameraHeight(int cameraHeight) {
 		APokerServer.cameraHeight = cameraHeight;
 	}
 
@@ -101,19 +107,36 @@ public class APokerServer extends BaseGameActivity{
 
 	@Override
 	public void onLoadResources() {
-		
-		//Setting the path for graphics
+
+		//Set the path for graphics
 		BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("gfx/");
-		
+
+		//Load the background texture
+		mBackgroundTextureAtlas = new BitmapTextureAtlas(1024, 1024, TextureOptions.BILINEAR_PREMULTIPLYALPHA);
+		mBackgroundTexureRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(this.mBackgroundTextureAtlas, this,"gamebackground.png", 0, 0);
+		mEngine.getTextureManager().loadTexture(mBackgroundTextureAtlas);
+
+		// Extract the textures of each button
+		this.mButtonsTextureAtlas = new BitmapTextureAtlas(1024, 1024, TextureOptions.BILINEAR_PREMULTIPLYALPHA);
+
+		this.mButtonsTextureRegionMap = new HashMap<Button, TextureRegion>();
+
+		int i = 0;
+
+		for(final Button button : Button.values()){
+			final TextureRegion buttonTextureRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(this.mButtonsTextureAtlas, this, button.name()+".png", i*button.BUTTON_HEIGHT, i*button.BUTTON_WIDTH);
+			this.mButtonsTextureRegionMap.put(button, buttonTextureRegion);
+			i++;
+		}	
+
+		this.mEngine.getTextureManager().loadTexture(this.mButtonsTextureAtlas);
+
 		//Load the card deck into a TextureAtlas
 		this.mCardDeckTextureAtlas = new BitmapTextureAtlas(1024, 512, TextureOptions.BILINEAR_PREMULTIPLYALPHA);
-		
 		BitmapTextureAtlasTextureRegionFactory.createFromAsset(this.mCardDeckTextureAtlas, this, "carddeck_tiled.png", 0, 0);
 
 		//Load each card texture into a HashMap
 		this.mCardTotextureRegionMap = new HashMap<Card, TextureRegion>();
-
-		this.mBackgroundTexureRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(new BitmapTextureAtlas(1024, 1024, TextureOptions.BILINEAR_PREMULTIPLYALPHA), this, "menubackground.png", 0, 0);
 
 		/* Extract the TextureRegion of each card in the whole deck. */
 		for(final Card card : Card.values()) {
@@ -123,30 +146,29 @@ public class APokerServer extends BaseGameActivity{
 
 		this.mEngine.getTextureManager().loadTexture(this.mCardDeckTextureAtlas);
 
-		mBackgroundTextureAtlas = new BitmapTextureAtlas(1024, 1024, TextureOptions.BILINEAR_PREMULTIPLYALPHA);
-		mBackgroundTexureRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(this.mBackgroundTextureAtlas, this,"gamebackground.png", 0, 0);
-		
-		mEngine.getTextureManager().loadTexture(mBackgroundTextureAtlas);
-
 		Log.i("APokerCLient", "onLoadResources correcto");
 	}
 
 	@Override
 	public Scene onLoadScene() {
-		
+
 		this.mEngine.registerUpdateHandler(new FPSLogger());
 
 		this.mMainScene = new Scene();
 		this.mMainScene.setOnAreaTouchTraversalFrontToBack();
 
-		this.addCard(Card.CLUB_ACE, 200, 100);
-		this.addCard(Card.HEART_ACE, 200, 260);
-		this.addCard(Card.DIAMOND_ACE, 440, 100);
-		this.addCard(Card.SPADE_ACE, 440, 260);
-
 		backgroundSpriteSprite = new Sprite(0, 0, mBackgroundTexureRegion);
 		backgroundSprite = new SpriteBackground(backgroundSpriteSprite);
-		
+
+		mMainScene.setBackground(backgroundSprite);
+
+		//		this.addCard(Card.CLUB_ACE, 200, 100);
+		//		this.addCard(Card.HEART_ACE, 200, 260);
+		//		this.addCard(Card.DIAMOND_ACE, 440, 100);
+		//		this.addCard(Card.SPADE_ACE, 440, 260);
+
+		this.addButtons();
+
 		mMainScene.setBackground(backgroundSprite);
 
 		this.mMainScene.setTouchAreaBindingEnabled(true);
@@ -164,11 +186,40 @@ public class APokerServer extends BaseGameActivity{
 	// Methods
 	// ===========================================================
 
-	/**
-	 * 
-	 */
 	private void addCard(final Card pCard, final int pX, final int pY) {
-		final Sprite sprite = new Sprite(pX, pY, this.mCardTotextureRegionMap.get(pCard)) {
+		final Sprite sprite = new Sprite(pX, pY, this.mCardTotextureRegionMap.get(pCard));
+		//		{
+		//			boolean mGrabbed = false;
+		//
+		//			@Override
+		//			public boolean onAreaTouched(final TouchEvent pSceneTouchEvent, final float pTouchAreaLocalX, final float pTouchAreaLocalY) {
+		//				switch(pSceneTouchEvent.getAction()) {
+		//				case TouchEvent.ACTION_DOWN:
+		//					this.setScale(1.25f);
+		//					this.mGrabbed = true;
+		//					break;
+		//				case TouchEvent.ACTION_MOVE:
+		//					if(this.mGrabbed) {
+		//						this.setPosition(pSceneTouchEvent.getX() - Card.CARD_WIDTH / 2, pSceneTouchEvent.getY() - Card.CARD_HEIGHT / 2);
+		//					}
+		//					break;
+		//				case TouchEvent.ACTION_UP:
+		//					if(this.mGrabbed) {
+		//						this.mGrabbed = false;
+		//						this.setScale(1.0f);
+		//					}
+		//					break;
+		//				}
+		//				return true;
+		//			}
+		//		};
+
+		this.mMainScene.attachChild(sprite);
+		this.mMainScene.registerTouchArea(sprite);
+	}
+
+	private void addFoldButton(final int pX, final int pY){
+		final Sprite sprite = new Sprite(pX, pY, this.mButtonsTextureRegionMap.get(Button.FOLD)){
 			boolean mGrabbed = false;
 
 			@Override
@@ -177,11 +228,6 @@ public class APokerServer extends BaseGameActivity{
 				case TouchEvent.ACTION_DOWN:
 					this.setScale(1.25f);
 					this.mGrabbed = true;
-					break;
-				case TouchEvent.ACTION_MOVE:
-					if(this.mGrabbed) {
-						this.setPosition(pSceneTouchEvent.getX() - Card.CARD_WIDTH / 2, pSceneTouchEvent.getY() - Card.CARD_HEIGHT / 2);
-					}
 					break;
 				case TouchEvent.ACTION_UP:
 					if(this.mGrabbed) {
@@ -193,9 +239,119 @@ public class APokerServer extends BaseGameActivity{
 				return true;
 			}
 		};
-
 		this.mMainScene.attachChild(sprite);
 		this.mMainScene.registerTouchArea(sprite);
+	}
+
+	private void addCheckButton(final int pX, final int pY){
+		final Sprite sprite = new Sprite(pX, pY, this.mButtonsTextureRegionMap.get(Button.CHECK)){
+			boolean mGrabbed = false;
+
+			@Override
+			public boolean onAreaTouched(final TouchEvent pSceneTouchEvent, final float pTouchAreaLocalX, final float pTouchAreaLocalY) {
+				switch(pSceneTouchEvent.getAction()) {
+				case TouchEvent.ACTION_DOWN:
+					this.setScale(1.25f);
+					this.mGrabbed = true;
+					break;
+				case TouchEvent.ACTION_UP:
+					if(this.mGrabbed) {
+						this.mGrabbed = false;
+						this.setScale(1.0f);
+					}
+					break;
+				}
+				return true;
+			}
+		};
+		this.mMainScene.attachChild(sprite);
+		this.mMainScene.registerTouchArea(sprite);
+	}
+
+	private void addCallButton(final int pX, final int pY){
+		final Sprite sprite = new Sprite(pX, pY, this.mButtonsTextureRegionMap.get(Button.CALL)){
+			boolean mGrabbed = false;
+
+			@Override
+			public boolean onAreaTouched(final TouchEvent pSceneTouchEvent, final float pTouchAreaLocalX, final float pTouchAreaLocalY) {
+				switch(pSceneTouchEvent.getAction()) {
+				case TouchEvent.ACTION_DOWN:
+					this.setScale(1.25f);
+					this.mGrabbed = true;
+					break;
+				case TouchEvent.ACTION_UP:
+					if(this.mGrabbed) {
+						this.mGrabbed = false;
+						this.setScale(1.0f);
+					}
+					break;
+				}
+				return true;
+			}
+		};
+		this.mMainScene.attachChild(sprite);
+		this.mMainScene.registerTouchArea(sprite);
+	}
+
+	private void addRaiseButton(final int pX, final int pY){
+		final Sprite sprite = new Sprite(pX, pY, this.mButtonsTextureRegionMap.get(Button.RAISE)){
+			boolean mGrabbed = false;
+
+			@Override
+			public boolean onAreaTouched(final TouchEvent pSceneTouchEvent, final float pTouchAreaLocalX, final float pTouchAreaLocalY) {
+				switch(pSceneTouchEvent.getAction()) {
+				case TouchEvent.ACTION_DOWN:
+					this.setScale(1.25f);
+					this.mGrabbed = true;
+					break;
+				case TouchEvent.ACTION_UP:
+					if(this.mGrabbed) {
+						this.mGrabbed = false;
+						this.setScale(1.0f);
+					}
+					break;
+				}
+				return true;
+			}
+		};
+		this.mMainScene.attachChild(sprite);
+		this.mMainScene.registerTouchArea(sprite);
+	}
+
+	private void addExitButton(final int pX, final int pY){
+		final Sprite sprite = new Sprite(pX, pY, this.mButtonsTextureRegionMap.get(Button.EXIT)){
+			boolean mGrabbed = false;
+
+			@Override
+			public boolean onAreaTouched(final TouchEvent pSceneTouchEvent, final float pTouchAreaLocalX, final float pTouchAreaLocalY) {
+				switch(pSceneTouchEvent.getAction()) {
+				case TouchEvent.ACTION_DOWN:
+					this.setScale(1.25f);
+					this.mGrabbed = true;
+					break;
+				case TouchEvent.ACTION_UP:
+					if(this.mGrabbed) {
+						this.mGrabbed = false;
+						this.setScale(1.0f);
+						finish();
+					}
+					break;
+				}
+				return true;
+			}
+		};
+		this.mMainScene.attachChild(sprite);
+		this.mMainScene.registerTouchArea(sprite);
+	}
+
+	private void addButtons() {
+
+		this.addFoldButton(0, cameraWidth - this.mButtonsTextureRegionMap.get(Button.FOLD).getWidth());
+		this.addCheckButton(this.mButtonsTextureRegionMap.get(Button.CHECK).getHeight(), cameraWidth - this.mButtonsTextureRegionMap.get(Button.CHECK).getWidth()+10);
+		this.addCallButton( cameraHeight/2 + this.mButtonsTextureRegionMap.get(Button.CALL).getHeight(),cameraWidth - this.mButtonsTextureRegionMap.get(Button.CALL).getWidth());
+		this.addRaiseButton(cameraHeight - this.mButtonsTextureRegionMap.get(Button.RAISE).getHeight(), cameraWidth/2 + this.mButtonsTextureRegionMap.get(Button.RAISE).getWidth()*2 + 10);
+		this.addExitButton(0, cameraWidth - this.mButtonsTextureRegionMap.get(Button.EXIT).getWidth());
+
 	}
 
 	// ===========================================================
